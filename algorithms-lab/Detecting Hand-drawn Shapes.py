5a7def380b5f4d4d0b7b214cc6a264607e3afa00
ManbirJudge <manbirjudge2009@gmail.com>
2023-09-03 17:29:11 +0530

    Update

---
+++ algorithms-lab/Detecting Hand-drawn Shapes.py
@@ -0,0 +1,35 @@
+# imports
+import cv2
+import numpy as np
+
+# reading and preprocessing
+img = cv2.imread('images/istockphoto-950742476-612x612.jpg')
+img_updated = img.copy()
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
+
+# detecting lines
+lines = []
+lines_ = cv2.HoughLinesP(
+    edges,              # input edge image
+    1,                  # distance resolution (in pixels)
+    np.pi / 180,        # angle resolution (in radians)
+    threshold=100,      # min number of votes for valid line
+    minLineLength=5,    # min allowed length of line
+    maxLineGap=10       # max allowed gap between line for joining them
+)
+
+for line in lines_:
+    x1, y1, x2, y2 = line[0]
+
+    cv2.line(img_updated, (x1, y1), (x2, y2), (56, 230, 7), 2)
+
+    lines.append([(x1, y1), (x2, y2)])
+
+# showing the result
+cv2.imshow('Original', img)
+cv2.imshow('Gray', gray)
+cv2.imshow('Edges (Canny)', edges)
+cv2.imshow('Lines', img_updated)
+
+cv2.waitKey(0)
